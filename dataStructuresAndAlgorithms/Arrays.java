5283ed4beb46dd3df57d06a57acd3411baa7d865
aswingithub2020 <suvanam.aswin2011@gmail.com>
2025-07-11 07:30:21 +0530

    Modified Array code

--- dataStructuresAndAlgorithms/Arrays.java
+++ dataStructuresAndAlgorithms/Arrays.java
@@ -10,22 +10,53 @@ public class Arrays {
 			System.out.println("Array values at index " + i + " is " + arr[i]);
 		}
 	}
-	
-	//Multi-Dimensional Array
+
+	// Multi-Dimensional Array
 	void multiDimensionalArray() {
-		int arr[][]= {{1,2,3},{2,3,4},{4,5,6}};
-		
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr[i].length;j++) {
-				System.out.println("Array Element at position "+i+" is "+arr[i][j]);
+		int arr[][] = { { 1, 2, 3 }, { 2, 3, 4 }, { 4, 5, 6 } };
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.println("Array Element at position " + i + " is " + arr[i][j]);
 			}
 		}
-		
+	}
+
+	// Single Dimensional Array with return
+	int[] singleDimensionalArrayWithReturn() {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+		return arr;
+	}
+
+	// Multi Dimensional Array with return
+	int[][] multiDimensionalArrayWithReturn() {
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
+		return arr;
+	}
+
+	static void utilise1DArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("{Utilised} Array Element at index " + i + " is " + arr[i]);
+		}
+	}
+
+	static void utiliseMultiDArray(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.println("{Utilised} Array Element at position " + i + " is " + arr[i][j]);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		Arrays arrayIteration = new Arrays();
-		//arrayIteration.singleDimensionalArray();
-		arrayIteration.multiDimensionalArray();
+		// arrayIteration.singleDimensionalArray();
+		// arrayIteration.multiDimensionalArray();
+
+		// Utilise the returned 1-D array in another method
+		//utilise1DArray(arrayIteration.singleDimensionalArrayWithReturn());
+
+		// Utilise the returned Multi-D array in another method
+		utiliseMultiDArray(arrayIteration.multiDimensionalArrayWithReturn());
 	}
 }
